9e74ee7c972983451c68dfab10c8a25274e485cf
Adam McElwee <adam@mcelwee.me>
2013-08-21 10:53:17 -0500

    Restructured and simplified. Removed commonjs

--- src/jquery.responsiveiframe.js
+++ src/jquery.responsiveiframe.js
@@ -1,110 +1,108 @@
-if (typeof jQuery !== 'undefined') {
-  (function( $ ){
-    var settings = {
-      xdomain: '*',
-      scrollToTop: true
-    };
-
-    var methods = {
-      // initialization for the parent, the one housing this
-      init: function() {
-        return this.each(function(self){
-          var $this = $(this);
-
-          if (window.postMessage) {
-            if (window.addEventListener) {
-              window.addEventListener('message', function(e) {
-                privateMethods.messageHandler($this,e);
-              } , false);
-            } else if (window.attachEvent) {
-              window.attachEvent('onmessage', function(e) {
-                privateMethods.messageHandler($this,e);
-              }, $this);
-            }
-          }
-        });
-      }
-    };
-
-    var privateMethods = {
-      messageHandler: function (elem, e) {
-        var height,
-          r,
-          matches,
-          strD;
-
-        if (settings.xdomain !== '*') {
-          var regex = new RegExp(settings.xdomain + '$');
-          if(e.orgin == "null"){
-            throw new Error("messageHandler( elem, e): There is no origin.  You are viewing the page from your file system.  Please run through a web server.");
-          }
-          if(e.origin.match(regex)){
-            matches = true;
-          }else{
-            throw new Error("messageHandler( elem, e): The origin doesn't match the responsiveiframe  xdomain.");
-          }
-        }
-
-        if(settings.xdomain === '*' || matches ) {
-          strD = e.data + "";
-          r = strD.match(/^(\d+)(s?)$/);
-          if(r && r.length === 3){
-            height = parseInt(r[1], 10);
-            if (!isNaN(height)) {
-              try {
-                privateMethods.setHeight(elem, height);
-              } catch (ex) {}
-            }
-            if (settings.scrollToTop && r[2] === "s"){
-              scroll(0,0);
-            }
-          }
-        }
-      },
-
-      // Sets the height of the iframe
-      setHeight : function (elem, height) {
-        elem.css('height', height + 'px');
-      }
-    };
-
-    $.fn.responsiveIframe = function( method ) {
-      if ( methods[method] ) {
-        return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
-      } else if ( typeof method === 'object' || ! method ) {
-        $.extend(settings, arguments[0]);
-        return methods.init.apply( this );
-      } else {
-        $.error( 'Method ' +  method + ' does not exist on jQuery.responsiveIframe' );
-      }
-    };
-  }( jQuery ));
-}
-
-;(function(){
-  var ResponsiveIframe = function () {},
-      defaults = {
-        resizeOnly: true,
-        scrollToTop: false,
-        pollInterval: 150
+(function() {
+  if (typeof jQuery !== 'undefined') {
+    (function( $ ){
+      var settings = {
+        xdomain: '*',
+        scrollToTop: true
       };
 
-  function getDocHeight() {
-    var body = document.body,
-       html = document.documentElement;
+      var methods = {
+        // initialization for the parent, the one housing this
+        init: function() {
+          return this.each(function(self){
+            var $this = $(this);
 
-    return Math.max( body.scrollHeight, body.offsetHeight,
-                       html.clientHeight, html.scrollHeight, html.offsetHeight );
+            if (window.postMessage) {
+              if (window.addEventListener) {
+                window.addEventListener('message', function(e) {
+                  privateMethods.messageHandler($this,e);
+                } , false);
+              } else if (window.attachEvent) {
+                window.attachEvent('onmessage', function(e) {
+                  privateMethods.messageHandler($this,e);
+                }, $this);
+              }
+            }
+          });
+        }
+      };
+
+      var privateMethods = {
+        messageHandler: function (elem, e) {
+          var height,
+            r,
+            matches,
+            strD;
+
+          if (settings.xdomain !== '*') {
+            var regex = new RegExp(settings.xdomain + '$');
+            if(e.orgin == "null"){
+              throw new Error("messageHandler( elem, e): There is no origin.  You are viewing the page from your file system.  Please run through a web server.");
+            }
+            if(e.origin.match(regex)){
+              matches = true;
+            }else{
+              throw new Error("messageHandler( elem, e): The origin doesn't match the responsiveiframe  xdomain.");
+            }
+          }
+
+          if(settings.xdomain === '*' || matches ) {
+            strD = e.data + "";
+            r = strD.match(/^(\d+)(s?)$/);
+            if(r && r.length === 3){
+              height = parseInt(r[1], 10);
+              if (!isNaN(height)) {
+                try {
+                  privateMethods.setHeight(elem, height);
+                } catch (ex) {}
+              }
+              if (settings.scrollToTop && r[2] === "s"){
+                scroll(0,0);
+              }
+            }
+          }
+        },
+
+        // Sets the height of the iframe
+        setHeight : function (elem, height) {
+          elem.css('height', height + 'px');
+        }
+      };
+
+      $.fn.responsiveIframe = function( method ) {
+        if ( methods[method] ) {
+          return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
+        } else if ( typeof method === 'object' || ! method ) {
+          $.extend(settings, arguments[0]);
+          return methods.init.apply( this );
+        } else {
+          $.error( 'Method ' +  method + ' does not exist on jQuery.responsiveIframe' );
+        }
+      };
+    }( jQuery ));
   }
 
-  ResponsiveIframe.prototype.allowResponsiveEmbedding = function(opts) {
-    var previousHeight;
+  function ResponsiveIframe(opts){
+    var defaults = {
+          resizeOnly: true,
+          scrollToTop: false,
+          pollInterval: 150
+        },
+        previousHeight;
 
     opts = opts || {};
     for (var k in defaults) {
       if (defaults.hasOwnProperty(k) && !opts.hasOwnProperty(k)) {
         opts[k] = defaults[k];
       }
+    }
+
+    function getDocHeight() {
+      var body = document.body,
+         html = document.documentElement;
+
+      return Math.max( body.scrollHeight, body.offsetHeight,
+                         html.clientHeight, html.scrollHeight, html.offsetHeight );
     }
 
     function messageParent(newHeight) {
@@ -132,26 +130,19 @@ if (typeof jQuery !== 'undefined') {
       }
     }
 
-    if (window.addEventListener) {
-      window.addEventListener("load", firstLoad, false);
-    } else if (window.attachEvent) {
-      window.attachEvent("onload", firstLoad);
-    }
-  };
-
-  function responsiveIframe() {
-    return new ResponsiveIframe();
+    firstLoad();
   }
 
   // expose
   if (typeof define === 'function' && define.amd) {
     // AMD
-    define(responsiveIframe);
-  } else if (typeof module !== 'undefined' && module.exports) {
-    // CommonJS
-    module.exports.responsiveIframe = responsiveIframe;
+    define(function() {
+      return ResponsiveIframe;
+    });
   } else {
     // Drop onto window
-    window.responsiveIframe = responsiveIframe;
+    window.ResponsiveIframe = ResponsiveIframe;
   }
-}());
+
+  return ResponsiveIframe;
+})();
